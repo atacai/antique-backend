425940a7c13ef89e67dc1b007ce9314ded77c2e8
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-08-27 19:34:26 +0700

    models: to add auto creation of bid based on auto bid config

--- auction/models.py
+++ auction/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 
 from core.models import Product
@@ -27,3 +29,9 @@ class AutoBidConfig(models.Model):
 
     def __str__(self):
         return '%s-%s' % (self.user.username, self.product.name)
+
+
+@receiver(post_save, sender=Bid)
+def create_auto_bid(sender, instance, **kwargs):
+    for bid in AutoBidConfig.objects.filter(is_active=True, product=instance.product, max_amount__gte=instance.amount).exclude(user=instance.user).order_by('-id'):
+        Bid.objects.create(user=bid.user, product=instance.product, amount=instance.amount + bid.increment)
